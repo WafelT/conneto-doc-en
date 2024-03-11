9c9641bd08ad34c25d07f28905f8f60551313fd6
Dmitrii Turcanu <dumitru_turcanu@alsodev.md>
2024-03-11 09:05:14 +0200

    [update] add example of using comment TTS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,65 +1,158 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-/******/ (function() { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
+if (document.querySelector('#codeMapGetters')) {
+document.querySelector('#codeMapGetters').value = 
+`import { mapGetters } from 'vuex'
 
-/***/ "./src/assets/js/app.js":
-/*!******************************!*\
-  !*** ./src/assets/js/app.js ***!
-  \******************************/
-/***/ (function() {
+computed: {
+    ...mapGetters({
+        exampleData: 'example/exampleData',
+        exampleDataSecond: 'example/exampleDataSecond',
+    })
+}`
+}
 
-eval("if (document.querySelector('#codeMapGetters')) {\r\ndocument.querySelector('#codeMapGetters').value = \r\n`import { mapGetters } from 'vuex'\r\n\r\ncomputed: {\r\n    ...mapGetters({\r\n        exampleData: 'example/exampleData',\r\n        exampleDataSecond: 'example/exampleDataSecond',\r\n    })\r\n}`\r\n}\r\n\r\nif (document.querySelector('#codeMapAction')) {\r\ndocument.querySelector('#codeMapAction').value = \r\n`this.$store.dispatch('example/exampleActionFunction', data)`\r\n}\r\n\r\nif (document.querySelector('#codeStoreMutation')) {\r\ndocument.querySelector('#codeStoreMutation').value = \r\n`mutations: {\r\n    SET_EXAMPLE_DATA: (state, data) => state.exampleData = data,\r\n},`\r\n}\r\n\r\nif (document.querySelector('#codeComponentImports')) {\r\ndocument.querySelector('#codeComponentImports').value = \r\n`components: {\r\n    HeaderDeleted: () => import('~/components/common/the-header/HeaderDeleted.vue'),\r\n    \r\n    ContractsIcon: () => import('~/components/common/icons/ContractsIcon.vue'),\r\n    DeletedFilesIcon: () => import('~/components/common/icons/DeletedFilesIcon.vue'),\r\n    FolderIcon: () => import('~/components/common/icons/FolderIcon.vue'),\r\n    ClipIcon: () => import('~/components/common/icons/ClipIcon.vue'),\r\n\r\n    SkeletonProjectsFolder: () => import('~/components/common/skeletons/SkeletonProjectsFolder.vue'),\r\n},`\r\n}\r\n\r\nif (document.querySelector('#codeLanguageImport')) {\r\ndocument.querySelector('#codeLanguageImport').value = \r\n`export default {\r\n    contractSelectHeaderCategory: 'Category',\r\n},`\r\n}\r\n\r\nif (document.querySelector('#codeEndPointTemplate')) {\r\n    document.querySelector('#codeEndPointTemplate').value = \r\n    \"const res = await this.$axios({\\n\" +\r\n    \"    method: 'GET',\\n\" +\r\n    \"    url: `/api/v2/example/`,\\n\" +\r\n    \"    headers: { Authorization: `Bearer ${this.$cookies.get('token')}` },\\n\" +\r\n    \"})\\n\" +\r\n    \"commit('SET_DATA', res.data)\"\r\n}\r\n\r\nif (document.querySelector('#codePropsTemplate')) {\r\ndocument.querySelector('#codePropsTemplate').value = \r\n`props: {\r\n    value: VueTypes.oneOfType([String, Object, Number]).def(''),\r\n    pagination: VueTypes.object.def(null),\r\n    pagination: VueTypes.object,\r\n    text: VueTypes.string.required,\r\n    isCleared: VueTypes.bool.def(false),\r\n},`\r\n}\r\n\r\nif (document.querySelector('#codeIconsTemplate')) {\r\ndocument.querySelector('#codeIconsTemplate').value = \r\n`<template>\r\n    <svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\r\n        <path d=\"M3 10\" class=\"i-background i-background--stroke i-background--fill\" stroke-width=\"2\" />\r\n        <path d=\"dz\" fill=\"#1D2939\" stroke=\"#1D2939\" stroke-width=\"2\"/>\r\n    </svg>\r\n</template>\r\n\r\n<script>\r\n    export default {\r\n        name: 'ExampleIcon',\r\n    }\r\n</script>`\r\n}\r\n\r\n\r\nif (document.querySelector('#codeToastMessagesTemplate')) {\r\ndocument.querySelector('#codeToastMessagesTemplate').value = \r\n`<template>\r\n    <MyToast\r\n        :name=\"messageExampleIdentificatorName\"\r\n        :message=\"'<strong>Example text of toast message</strong>'\"\r\n        :duration=\"5000\"\r\n        :type=\"'error'\"\r\n        :position=\"'top-center'\"\r\n    />\r\n</template>\r\n\r\n<script>\r\n    export default {\r\n        methods: {\r\n            showToastMessage(name) {\r\n                eventBus.$emit('show-toast', name)\r\n            },\r\n        },\r\n    }\r\n</script>`\r\n}\n\n//# sourceURL=webpack://wafelt/./src/assets/js/app.js?");
+if (document.querySelector('#codeMapAction')) {
+document.querySelector('#codeMapAction').value = 
+`this.$store.dispatch('example/exampleActionFunction', data)`
+}
 
-/***/ }),
+if (document.querySelector('#codeStoreMutation')) {
+document.querySelector('#codeStoreMutation').value = 
+`mutations: {
+    SET_EXAMPLE_DATA: (state, data) => state.exampleData = data,
+},`
+}
 
-/***/ "./src/assets/js/imask.js":
-/*!********************************!*\
-  !*** ./src/assets/js/imask.js ***!
-  \********************************/
-/***/ (function() {
+if (document.querySelector('#codeComponentImports')) {
+document.querySelector('#codeComponentImports').value = 
+`components: {
+    HeaderDeleted: () => import('~/components/common/the-header/HeaderDeleted.vue'),
+    
+    ContractsIcon: () => import('~/components/common/icons/ContractsIcon.vue'),
+    DeletedFilesIcon: () => import('~/components/common/icons/DeletedFilesIcon.vue'),
+    FolderIcon: () => import('~/components/common/icons/FolderIcon.vue'),
+    ClipIcon: () => import('~/components/common/icons/ClipIcon.vue'),
 
-eval("const tels = document.querySelectorAll(\"input[type=\\\"tel\\\"]\");\r\n\r\nif (tels) {\r\n    tels.forEach(el => {\r\n        IMask(el, { mask: \"+ {0} (000) 000 00 000 00\" });\r\n    });\r\n}\n\n//# sourceURL=webpack://wafelt/./src/assets/js/imask.js?");
+    SkeletonProjectsFolder: () => import('~/components/common/skeletons/SkeletonProjectsFolder.vue'),
+},`
+}
 
-/***/ }),
+if (document.querySelector('#codeLanguageImport')) {
+document.querySelector('#codeLanguageImport').value = 
+`export default {
+    contractSelectHeaderCategory: 'Category',
+},`
+}
 
-/***/ "./src/assets/js/lozad.js":
-/*!********************************!*\
-  !*** ./src/assets/js/lozad.js ***!
-  \********************************/
-/***/ (function() {
+if (document.querySelector('#codeEndPointTemplate')) {
+    document.querySelector('#codeEndPointTemplate').value = 
+    "const res = await this.$axios({\n" +
+    "    method: 'GET',\n" +
+    "    url: `/api/v2/example/`,\n" +
+    "    headers: { Authorization: `Bearer ${this.$cookies.get('token')}` },\n" +
+    "})\n" +
+    "commit('SET_DATA', res.data)"
+}
 
-eval("const observer = lozad('.lozad', {\r\n    loaded: function(img) {\r\n        img.addEventListener('load', function() {\r\n            img.classList.add('loaded');\r\n        });\r\n        \r\n        if(img.hasAttribute('data-background-image')) {\r\n            img.classList.add('loaded');\r\n        };\r\n    },\r\n});\r\n\r\n\r\nobserver.observe();\n\n//# sourceURL=webpack://wafelt/./src/assets/js/lozad.js?");
+if (document.querySelector('#codePropsTemplate')) {
+document.querySelector('#codePropsTemplate').value = 
+`props: {
+    value: VueTypes.oneOfType([String, Object, Number]).def(''),
+    pagination: VueTypes.object.def(null),
+    pagination: VueTypes.object,
+    text: VueTypes.string.required,
+    isCleared: VueTypes.bool.def(false),
+},`
+}
 
-/***/ }),
+if (document.querySelector('#codeIconsTemplate')) {
+document.querySelector('#codeIconsTemplate').value = 
+`<template>
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M3 10" class="i-background i-background--stroke i-background--fill" stroke-width="2" />
+        <path d="dz" fill="#1D2939" stroke="#1D2939" stroke-width="2"/>
+    </svg>
+</template>
 
-/***/ "./src/assets/js/smooth-links.js":
-/*!***************************************!*\
-  !*** ./src/assets/js/smooth-links.js ***!
-  \***************************************/
-/***/ (function() {
+<script>
+    export default {
+        name: 'ExampleIcon',
+    }
+</script>`
+}
 
-eval("const smoothLinks = document.querySelectorAll('a[href^=\"#\"]');\r\nif (smoothLinks) {\r\n    for (let smoothLink of smoothLinks) {\r\n        smoothLink.addEventListener('click', function (e) {\r\n            e.preventDefault();\r\n            const id = smoothLink.getAttribute('href');\r\n    \r\n            if (id === \"#\") return\r\n            document.querySelector(id).scrollIntoView({\r\n                behavior: 'smooth',\r\n                block: 'start'\r\n            });\r\n        });\r\n    };\r\n}\n\n//# sourceURL=webpack://wafelt/./src/assets/js/smooth-links.js?");
 
-/***/ })
+if (document.querySelector('#codeToastMessagesTemplate')) {
+document.querySelector('#codeToastMessagesTemplate').value = 
+`<template>
+    <MyToast
+        :name="messageExampleIdentificatorName"
+        :message="'<strong>Example text of toast message</strong>'"
+        :duration="5000"
+        :type="'error'"
+        :position="'top-center'"
+    />
+</template>
 
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	__webpack_modules__["./src/assets/js/app.js"]();
-/******/ 	__webpack_modules__["./src/assets/js/imask.js"]();
-/******/ 	__webpack_modules__["./src/assets/js/lozad.js"]();
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/assets/js/smooth-links.js"]();
-/******/ 	
-/******/ })()
-;
+<script>
+    export default {
+        methods: {
+            showToastMessage(name) {
+                eventBus.$emit('show-toast', name)
+            },
+        },
+    }
+</script>`
+}
+
+if (document.querySelector('#codeExampleUsingLocalizationCommentLogic')) {
+document.querySelector('#codeExampleUsingLocalizationCommentLogic').value = 
+`else if(this.form.invitedUsers.includes(this.profileData.email)) {
+    this.messages.push("You have entered the document owner's email.") // TTS
+    return
+}`
+}
+    
+if (document.querySelector('#codeExampleUsingLocalizationCommentLayout')) {
+document.querySelector('#codeExampleUsingLocalizationCommentLayout').value = 
+`<!-- TTS -->
+<h4>The Title of Modal</h4>
+<button @click="closeModal()" class="modal__content-header-close">
+    <svg><use xlink:href="@/assets/images/svg/sprite.svg#closeIcon"></use></svg>
+</button>`
+}
+const tels = document.querySelectorAll("input[type=\"tel\"]");
+
+if (tels) {
+    tels.forEach(el => {
+        IMask(el, { mask: "+ {0} (000) 000 00 000 00" });
+    });
+}
+const observer = lozad('.lozad', {
+    loaded: function(img) {
+        img.addEventListener('load', function() {
+            img.classList.add('loaded');
+        });
+        
+        if(img.hasAttribute('data-background-image')) {
+            img.classList.add('loaded');
+        };
+    },
+});
+
+
+observer.observe();
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+if (smoothLinks) {
+    for (let smoothLink of smoothLinks) {
+        smoothLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            const id = smoothLink.getAttribute('href');
+    
+            if (id === "#") return
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
+}
